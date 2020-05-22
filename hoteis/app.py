3a9ca9074ce9c6007416f18b76dadf6e44ac5f7f
GabrielCorreia <gabrielmal38@gmail.com.br>
2020-05-22 12:35:15 -0300

    Utilizando , uma lista de dicionários para aumentar um pouco mais o nivél do app, sendo que

--- hoteis/app.py
+++ hoteis/app.py
@@ -4,11 +4,39 @@ from flask import Flask
 #chamandoa biblioteca flask_restful e importando dela "Resource", e "Api"
 from flask_restful import Resource, Api
 
-#criando uma variavél por padrão "app", e vamos passar o flask para ela junto de f(__name__)
+#criando uma variavél por padrão "app", e vamos passar o flask para ela junto de (__name__)
 app = Flask(__name__)
-#criando agora uma variavél para Api
+#criando agora uma variavél para api, para instanciar a "Api"
 api = Api(app)
 
+#criando uma lista de hoteis para ser usada em nossa aplicação Api
+#logo a meta é trazer está lista de um banco de  dados ,ao invés de formato json,como é
+# oque estou fazendo.
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrela': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Curitiba'
+    },
+
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'estrela': 4.4,
+        'diaria': 380,
+        'cidade': 'Florianópolis'
+    },
+
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrela': 4.0,
+        'diaria': 390,
+        'cidade': 'Porto Alegre'
+    }
+]
 #criando uma classe hoteis
 # Esta primeira classe leva em seus (), Resource porque vai ser um recurso da nossa Api
 # este recurso como todos os recursos devem, conter Get Post e Delete
@@ -17,7 +45,7 @@ class Hoteis(Resource):
     def get(self):
         # aqui está sendo feito um dicionário,porém quando for feito a requisição, a biblioteca
         #flask_restful, vai retornar automaticamente nos retornar em formato json
-        return {'hoteis': 'meus hoteis'}
+        return {'hoteis': hoteis}
 
 
 
