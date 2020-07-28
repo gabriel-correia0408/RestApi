32f9ba2e34b28c846e415763422637efc01c6787
GabrielCorreia <gabrielmal38@gmail.com.br>
2020-07-28 11:44:43 -0300

    colocado , linha de código que traz no arquivo "banco", as tabelas do hotél criado

--- cria_banco.py
+++ cria_banco.py
@@ -8,8 +8,11 @@ criar_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY,\
 
 cria_hotel = "INSERT INTO hoteis VALUES ('alpha', 'alpha hotel', 4.3, 600.50, 'Blumenau')"
 
+seleciona_hoteis = "SELECT * FROM hoteis"
+
 cursor.execute(criar_tabela)
 cursor.execute(cria_hotel)
+cursor.execute(seleciona_hoteis)
 
 connection.commit()
 connection.close()
